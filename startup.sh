d8ae1641425f913871a95b480b499ce25d429773
ahmedstefan1 <34337955+ahmedstefan1@users.noreply.github.com>
2019-06-18 22:07:55 +0200

    sed werkt nog niet

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add
-echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" > /etc/apt/sources.list.d/saltstack.list
-sudo apt-get install salt-minion
-sed -i 's/#master: salt/master: 23.97.161.74/' /etc/salt/minion
+echo $(apt-get update)
+echo $(apt-get --assume-yes upgrade)
+echo $(wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add)
+echo $(echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" > /etc/apt/sources.list.d/saltstack.list)
+echo $(apt-get -y install salt-minion)
+$("sed -i 's/#master: salt/master: 23.97.161.74/' /etc/salt/minion")
+echo "finish"
