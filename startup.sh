4609bc3e4969874f1d5334ddb59a8afd194a8685
ahmedstefan1 <34337955+ahmedstefan1@users.noreply.github.com>
2019-06-30 14:41:15 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo $(apt-get update)
 echo $(apt-get --assume-yes upgrade)
 echo $(wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add)
-echo $(echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" > /etc/apt/sources.list.d/saltstack
+echo $(echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" > /etc/apt/sources.list.d/saltstack)
 $echo $(apt-get -y install salt-minion)
 echo $(sed -i 's/#master: salt/master: 23.97.161.74/' /etc/salt/minion)
 echo "finish"
